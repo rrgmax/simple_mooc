597d49f144dd2ae68052c9a83e5289f3cdb4329d
Rogerio <rrgmax@bol.com.br>
2019-02-14 06:18:17 -0200

    Remove permalink from models.py file

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -34,11 +34,8 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
-    @models.permalink
-    def get_absolute_url(self):
-        return ('courses:details', (), {'slug': self.slug})
-
     class Meta:
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
         ordering = ['name']
+        
