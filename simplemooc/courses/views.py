2b05927d8de3753baffa9b1aacd402ae1f488bc3
Rogerio <rrgmax@bol.com.br>
2019-02-14 01:07:50 -0200

    Change the name functions into courses/views.py file

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def courses(request):
-    template_name = 'courses/courses.html'
-    return render 
+def index(request):
+    template_name = 'courses/index.html'
+    return render(request, template_name) 
