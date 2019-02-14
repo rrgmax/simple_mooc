bd7542bba38918e3d448ed10acf937bf5189a7af
Rogerio <rrgmax@bol.com.br>
2019-02-13 23:27:36 -0200

    Define string self.name into courses/models.py file

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -29,3 +29,6 @@ class Course(models.Model):
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
     objects = CourseManager()
+
+    def __str__(self):
+        return self.name
