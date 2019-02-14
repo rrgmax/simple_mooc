bf1b2b8016f714a8d24e4de57e8810952952c5f1
Rogerio <rrgmax@bol.com.br>
2019-02-14 01:47:32 -0200

    Fix the template_name variable to cursos/index.html

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    template_name = 'courses/index.html'
+    template_name = 'cursos/index.html'
     return render(request, template_name) 
